ac6b8453fac79ae81468b08178f867bf88555193
Joseph Constan <jcon5294@gmail.com>
2013-02-10 15:43:06 -0500

    minor file organizations

---
+++ WordSmith/NextRoundViewController.h
@@ -0,0 +1,16 @@
+//
+//  NextRoundViewController.h
+//  WordSmith
+//
+//  Created by Joseph Constan on 7/18/12.
+//  Copyright (c) 2012 Codevs, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NextRoundViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UIImageView *bgView;
+@property (strong, nonatomic) IBOutlet UIButton *playButton;
+
+@end
